26d4b85156f0548ed39020e60e4d562abeaaa497
jeanosaur <jkestrella@gbox.adnu.edu.ph>
2025-08-29 00:32:18 +0800

    initial commit

---
+++ Assets/SCRIPTS/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 5f;
+    private bool isGrounded;
+    private Rigidbody2D rigidBody;
+    
+    void Start()
+    {
+      rigidBody = GetComponent<Rigidbody2D>();  
+    }
+    
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rigidBody.linearVelocity = new Vector2(moveInput * speed, rigidBody.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocity.x, jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
